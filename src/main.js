7fc57c0854361cf2e40e9690ea603b8a15d9631b
FraCuboni <fracuboni@gmail.com>
2024-10-01 15:53:39 +0200

    first commit

---
+++ src/main.js
@@ -0,0 +1,11 @@
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import { router } from './router
+
+// Importa il CSS di Bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css'
+// Importa il JS di Bootstrap (opzionale, per componenti JavaScript di Bootstrap)
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+
+createApp(App).mount('#app')
