a605bd843eae14a6200528f861bf553270edff0f
FraCuboni <fracuboni@gmail.com>
2024-10-02 16:15:55 +0200

    feat: added dinamic print of tags and posts

--- src/pages/Posts.vue
+++ src/pages/Posts.vue
@@ -7,20 +7,47 @@ export default {
     data() {
         return {
             data: [], //I POST VENGONO SALVATI QUI
+            requestType : 'posts',
             store,
         };
     },
     methods: {
-    },
-    mounted() {
-        axios.get(store.apiURL + 'posts') // URL API
+        getAPI(){
+        axios.get(store.apiURL + this.requestType) // URL API
             .then(res => {
-                this.data = res.data.results.data; // A BUON FINE
-                console.log('data:', res); // LOG
+
+                // RICHIESTA POSTS
+                if(this.requestType == 'posts'){
+                    this.data = res.data.results.data; // A BUON FINE
+                    console.log('POST DATA:', res); // LOG
+
+                // RICHIESTA TYPES
+                }else if(this.requestType == 'types'){
+                    this.data = res.data; // A BUON FINE
+                    console.log('TYPES DATA:', res); // LOG
+
+                // RICHIESTA TAGS
+                }else if(this.requestType == 'tags'){
+                    this.data = res.data; // A BUON FINE
+                    console.log('TAGS DATA:', res); // LOG
+
+                // ELSE
+                }else{
+                    console.log('wrong requestType value');
+                }
             })
             .catch(err => {
                 console.log('Errore nel recupero dei dati:', err); // LOG ERRORE
             });
+        },
+
+        changeType(value){
+            this.requestType = value;
+            this.getAPI();
+        }
+    },
+    mounted() {
+        this.getAPI();
     },
 };
 </script>
@@ -36,22 +63,61 @@ export default {
                     <div class="card-header">
                         <h2>posts</h2>
                     </div>
+                    
+                    <nav class="paginate" aria-label="Page navigation example">
+                        <ul class="pagination">
+                            <li class="page-item">
+                            <a class="page-link" href="#" aria-label="Previous">
+                                <span aria-hidden="true">&laquo;</span>
+                            </a>
+                            </li>
+                            <li @click="changeType('posts')" class="page-item"><a class="page-link" href="#">posts</a></li>
+                            <li @click="changeType('types')" class="page-item"><a class="page-link" href="#">types</a></li>
+                            <li @click="changeType('tags')" class="page-item"><a class="page-link" href="#">tags</a></li>
+                            <li class="page-item">
+                            <a class="page-link" href="#" aria-label="Next">
+                                <span aria-hidden="true">&raquo;</span>
+                            </a>
+                            </li>
+                        </ul>
+                    </nav>
 
                     <div class="bigcontainer">
-                        <div class="card" style="width: 18rem;" v-for="post in data">
+                        <div class="card" style="width: 18rem;" v-for="item in data">
+                            <img :src="item.image_path" class="card-img-top" alt="...">
                             <div class="card-body">
-                                <h5 class="card-title">#{{ post.id }}: {{ post.title }}</h5>
-                                <h6 class="card-subtitle mb-2 text-body-secondary">{{ post.slug }}</h6>
-                                <p class="card-text">{{ post.subject }}</p>
-                                <a href="#" class="card-link">{{ post.number_of_posts }}</a>
-                                <a href="#" class="card-link">{{ post.collaborators }}</a>
+                                <h5 class="card-title">#{{ item.id }}: {{ item.title }}{{ item.name }}</h5>
+                                <h6 class="card-subtitle mb-2 text-body-secondary">{{ item.slug }}</h6>
+                                <p class="card-text">{{ item.subject }}</p>
+                                <a href="#" class="card-link">{{ item.number_of_posts }}</a>
+                                <a href="#" class="card-link">{{ item.collaborators }}</a>
                             </div>
                         </div>
                     </div>
 
+                    <nav class="paginate" aria-label="Page navigation example">
+                        <ul class="pagination">
+                            <li class="page-item">
+                            <a class="page-link" href="#" aria-label="Previous">
+                                <span aria-hidden="true">&laquo;</span>
+                            </a>
+                            </li>
+                            <li class="page-item"><a class="page-link" href="#">1</a></li>
+                            <li class="page-item"><a class="page-link" href="#">2</a></li>
+                            <li class="page-item"><a class="page-link" href="#">3</a></li>
+                            <li class="page-item">
+                            <a class="page-link" href="#" aria-label="Next">
+                                <span aria-hidden="true">&raquo;</span>
+                            </a>
+                            </li>
+                        </ul>
+                    </nav>
+
                 </div>
             </div>
+            
         </div>
+        
     </div>
 </template>
 
@@ -63,6 +129,13 @@ export default {
 }
 
 .bigcontainer>.card {
-    margin: 15px;
+    margin: 10px;
+}
+
+.paginate{
+    margin-top: 20px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
 }
 </style>
